4fba8549a1b68c56a6738735b18a2f5c00225ae5
protechnopolis <146369803+protechnopolis@users.noreply.github.com>
2024-02-20 12:17:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const caracteres = Array.from({ length: 10 }, (_, i) => String.fromCharCode(48 +
     .concat(Array.from({ length: 26 }, (_, j) => String.fromCharCode(65 + j)))
     .concat(Array.from({ length: 26 }, (_, k) => String.fromCharCode(97 + k)));
 
-const webhookUrl = 'https://discord.com/api/webhooks/1209162417267810326/WwW3ZO785tYKtfPn1JGEh5Lee-nYCtWA7ErtN-h11tDtX0J1dwPdTUyErhxaRwNic63h'; // Remplacez ceci par l'URL complète de votre webhook
-
+const webhookUrl = 'YOUR_WEBHOOK_URL'; // PUT YOUR WEBHOOK URL HERE
 const generateNitroCode = () => {
     const nitrocode = Array.from({ length: 16 }, () => caracteres[Math.floor(Math.random() * caracteres.length)]).join('');
     return nitrocode;
@@ -14,9 +13,9 @@ const generateNitroCode = () => {
 const sendCodeToWebhook = async (nitrocode) => {
     try {
         await axios.post(webhookUrl, { content: `https://discord.gift/${nitrocode}` });
-        console.log(`Code Nitro envoyé avec succès à la webhook.`);
+        console.log(`Nitro code sended succesfully.`);
     } catch (error) {
-        console.error('Erreur lors de l\'envoi du code à la webhook:', error.message);
+        console.error('Error to with sending the nitro code:', error.message);
     }
 };
 
